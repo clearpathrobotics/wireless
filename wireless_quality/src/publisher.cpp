37d9abf26cd747778f71f8125a6e9c5fafb03a37
Mike Purvis <mpurvis@clearpathrobotics.com>
2013-03-25 14:05:09 -0400

    Add dynamic_reconfigure to the publisher.

--- wireless_quality/src/publisher.cpp
+++ wireless_quality/src/publisher.cpp
@@ -2,6 +2,25 @@
 #include "ros/ros.h"
 #include "sensor_msgs/CompressedImage.h"
 
+#include <dynamic_reconfigure/server.h>
+#include <wireless_quality/PublisherConfig.h>
+
+
+sensor_msgs::CompressedImage msg;
+ros::Publisher pub;
+ros::Timer pub_timer;
+
+void pub_callback(const ros::TimerEvent&) {
+    msg.header.stamp = ros::Time::now();
+    pub.publish(msg);
+}
+
+void config_callback(wireless_quality::PublisherConfig &config, uint32_t level)
+{ 
+    msg.data.resize(config.bytes_per_second / config.hz);
+    pub_timer.setPeriod(ros::Duration(1.0 / config.hz));
+}
+
 
 int main(int argc, char **argv)
 {
@@ -9,24 +28,25 @@ int main(int argc, char **argv)
 
     ros::NodeHandle nh;
 
-    int32_t hz, bytes_per_second, bytes_per_message;
+    pub_timer = nh.createTimer(ros::Duration(1.0), pub_callback);
 
-    // Publication rate 
-    nh.param<int32_t>("hz", hz, 10);
-    nh.param<int32_t>("bytes_per_second", bytes_per_second, 1000000);
-    bytes_per_message = bytes_per_second / hz;
+    dynamic_reconfigure::Server<wireless_quality::PublisherConfig> config_srv;
+    dynamic_reconfigure::Server<wireless_quality::PublisherConfig>::CallbackType f;
+    f = boost::bind(&config_callback, _1, _2);
+    config_srv.setCallback(f);
+    
+    //nh.param<int32_t>("hz", hz, 10);
+    //nh.param<int32_t>("bytes_per_second", bytes_per_second, 1000000);
+    //bytes_per_message = bytes_per_second / hz;
 
-    ros::Publisher pub = nh.advertise<sensor_msgs::CompressedImage>("data", 5);
+    pub = nh.advertise<sensor_msgs::CompressedImage>("data", 5);
 
-    sensor_msgs::CompressedImage msg;
-    msg.format = "dummy";
-    //msg.data.resize(bytes_per_message);
-
-    ros::Rate r(hz);
+    ros::spin();
+    /*ros::Rate r(hz);
     while (ros::ok())
     {
         msg.header.stamp = ros::Time::now();
         pub.publish(msg);
         r.sleep();
-    }
+    }*/
 }
--- wireless_quality/src/subscriber.cpp
+++ wireless_quality/src/subscriber.cpp
@@ -6,24 +6,52 @@
 
 
 wireless_msgs::Quality quality_msg;
+ros::Publisher quality_pub;
 
-void cb(const sensor_msgs::CompressedImage& msg) {
-    ROS_INFO("test!");
+void data_receive(const sensor_msgs::CompressedImage& msg) {
+    static int32_t last_seq = 0;
+
     ros::Duration latency = ros::Time::now() - msg.header.stamp;
     quality_msg.latency_measurements.push_back(latency.toSec());
+    quality_msg.messages_received++;
+    
+    quality_msg.message_lengths.push_back(msg.data.size()); 
+
+    quality_msg.messages_missed += (msg.header.seq - last_seq) - 1;
+    last_seq = msg.header.seq;
 }
 
-void cb_test(const std_msgs::Int32& msg) {
-    ROS_INFO("testy!");
-}
+void quality_publish(const ros::TimerEvent&) {
+    quality_msg.header.stamp = ros::Time::now();
 
+    uint32_t num_messages = quality_msg.messages_received;
+    quality_msg.total_length = 0;
+
+    if (num_messages > 0) {
+        double latency_total = 0;
+        for (size_t i = 0; i < num_messages; ++i)
+        {
+            latency_total += quality_msg.latency_measurements[i];
+            quality_msg.total_length += quality_msg.message_lengths[i];
+        }
+        quality_msg.latency_avg = latency_total / num_messages;
+    } else {
+        quality_msg.latency_avg = 0;
+    }
+
+    quality_pub.publish(quality_msg);
+    quality_msg.latency_measurements.resize(0);
+    quality_msg.message_lengths.resize(0);
+    quality_msg.messages_missed = 0;
+    quality_msg.messages_received = 0;
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "quality_subscriber");
 
     ros::NodeHandle nh;
-    ROS_INFO("pop!");
+    ROS_INFO("pop2!");
 
     double hz;
     bool unreliable;
@@ -33,17 +61,10 @@ int main(int argc, char **argv)
     nh.param<double>("hz", hz, 1.0);
 
     quality_msg.latency_measurements.reserve(30);
-    ros::Publisher quality_pub = nh.advertise<wireless_msgs::Quality>("quality", 5);
+    quality_pub = nh.advertise<wireless_msgs::Quality>("quality", 5);
 
-    ros::Subscriber sub = nh.subscribe("data", 1, cb);
-    ros::Subscriber sub2 = nh.subscribe("test", 1, cb_test);
+    ros::Subscriber data_sub = nh.subscribe("data", 1, data_receive, ros::TransportHints().unreliable());
+    ros::Timer quality_timer = nh.createTimer(ros::Duration(1.0 / hz), quality_publish);
 
-    ros::Rate r(hz);
-    while (ros::ok())
-    {
-        r.sleep();
-        quality_msg.header.stamp = ros::Time::now();
-        quality_pub.publish(quality_msg);
-        quality_msg.latency_measurements.resize(0);
-    }
+    ros::spin();
 }
